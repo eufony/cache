b51329c2921a3d5faec87ce1868361c6ca0ebfe5
Alpin H. Gencer <ah@gencer.us>
2022-08-03 13:04:36 +0200

    Add TagAwarePsr16Adapter

---
+++ src/Adapter/TagAwarePsr16Adapter.php
@@ -0,0 +1,69 @@
+<?php
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace Eufony\Cache\Adapter;
+
+use Eufony\Cache\TagAwareInterface;
+use Psr\Cache\CacheItemPoolInterface;
+
+/**
+ * Provides a wrapper class to adapt a PSR-6 caching implementation to support
+ * both the PSR-16 standards and `\Eufony\Cache\TagAwareInterface`.
+ *
+ * > **Deprecation notice:** This class is a temporary solution until a more
+ * > robust proxy management solution is implemented by the Eufony project.
+ *
+ * @deprecated
+ */
+class TagAwarePsr16Adapter extends Psr16Adapter implements TagAwareInterface
+{
+    /**
+     * {@inheritDoc}
+     *
+     * @var \Psr\Cache\CacheItemPoolInterface&\Eufony\Cache\TagAwareInterface $cache
+     */
+    protected CacheItemPoolInterface $cache;
+
+    /**
+     * {@inheritDoc}
+     *
+     * Additionally implements `\Eufony\Cache\TagAwareInterface`.
+     *
+     * @param \Psr\Cache\CacheItemPoolInterface $cache
+     *
+     * @deprecated
+     */
+    public function __construct(CacheItemPoolInterface $cache)
+    {
+        parent::__construct(new TagAwareAdapter($cache));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function tag(array|string $keys, string|array $tags): bool
+    {
+        return $this->cache->tag($keys, $tags);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function invalidateTags(string|array $tags): bool
+    {
+        return $this->cache->invalidateTags($tags);
+    }
+}
